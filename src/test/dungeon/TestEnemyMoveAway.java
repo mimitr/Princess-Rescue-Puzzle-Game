0b15a41cc06d4a9ca6625ece42888b948b060889
Mimi <mimitran1305@gmail.com>
2019-11-03 14:34:32 +1100

    test enemy

--- src/test/dungeon/TestEnemyMoveAway.java
+++ src/test/dungeon/TestEnemyMoveAway.java
@@ -18,17 +18,12 @@ class TestEnemyMoveAway {
 		Dungeon dungeon = new Dungeon(9, 9);
 		Player player = new Player(dungeon, 1, 1);
 		
-		Enemy e1 = new Enemy(dungeon, 1, 6);
-		Enemy e2 = new Enemy(dungeon, 3, 4);
-		Enemy e3 = new Enemy(dungeon, 5, 5);
-		Enemy e4 = new Enemy(dungeon, 6, 3);
-		Enemy e5 = new Enemy(dungeon, 7, 6);
+		Potion potion = new Potion(dungeon, 2, 1);
+		dungeon.addEntity(potion);
 		
-		dungeon.addEnemy(e1);
-		dungeon.addEnemy(e2);
-		dungeon.addEnemy(e3);
-		dungeon.addEnemy(e4);
-		dungeon.addEnemy(e5);
+		Enemy enemy = new Enemy(dungeon, 4, 2);
+		dungeon.addEnemy(enemy);
+		dungeon.addEntity(enemy);
 		
 		
 		// create vertical walls
@@ -54,9 +49,141 @@ class TestEnemyMoveAway {
 			dungeon.addEntity(wall5);
 		}
 		
+		Wall wall6 = new Wall(7, 2);
+		dungeon.addEntity(wall6);
+		
 		dungeon.setPlayer(player);
 		assertEquals(player.getX(), 1);
 		assertEquals(player.getY(), 1);
+		
+		/*
+		 * player picks up the potion
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 4);
+		assertEquals(enemy.getY(), 1);
+		
+		/*
+		 * now, the player has the potion and the enemy will move AWAY from the player
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 3);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 5);
+		assertEquals(enemy.getY(), 1);
+		
+		/*
+		 * the enemy is moving away from the player
+		 * potion.stepCounter = 1
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 4);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 6);
+		assertEquals(enemy.getY(), 1);
+		
+		/*
+		 * the enemy is moving away from the player
+		 * player (4,1) -> player(5, 1)
+		 * enemy(6,1) -> enemy(7, 1)
+		 * * potion.stepCounter = 2
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 5);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 7);
+		assertEquals(enemy.getY(), 1);
+		
+		/*
+		 * the enemy stays at the same place since he is blocked by another wall
+		 * * potion.stepCounter = 3
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 6);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 7);
+		assertEquals(enemy.getY(), 1);
+		
+		/*
+		 * the player catches the enemy while he's having the potion 
+		 * so the enemy dies and does not move anymore
+		 * * potion.stepCounter = 4
+		 */
+		player.moveRight();
+		assertEquals(player.getX(), 7);
+		assertEquals(player.getY(), 1);
+		assertEquals(enemy.getX(), 7);
+		assertEquals(enemy.getY(), 1);
+		assertEquals(enemy.stillAlive(), false);
+		
+		/*
+		 * potion.stepCounter = 5
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 6);
+		assertEquals(player.getY(), 1);
+		
+		/*
+		 * potion.stepCounter = 6
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 5);
+		assertEquals(player.getY(), 1);
+		
+		/*
+		 * potion.stepCounter = 7
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 4);
+		assertEquals(player.getY(), 1);
+		
+		/*
+		 * potion.stepCounter = 8
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 3);
+		assertEquals(player.getY(), 1);
+		
+		/*
+		 * potion.stepCounter = 9
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+		
+		/*
+		 * potion.stepCounter = 10
+		 */
+		player.moveLeft();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 1);
+		//assertEquals(player.getCarriedEntity(), potion);
+		assertEquals(potion.getX(), 2);
+		assertEquals(potion.getY(), 1);
+		
+		/*
+		 * after 10 steps with the potion, the potion stays at the same spot
+		 * but the player continues to move without the potion
+		 */
+		player.moveDown();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 2);
+		assertEquals(player.getCarriedEntity(), potion);
+
+		
+		player.moveDown();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 3);
+		assertEquals(player.getCarriedEntity(), potion);
+		
+		player.moveDown();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 4);
+		assertEquals(player.getCarriedEntity(), potion);
+		
+		
 	}
 
 }
